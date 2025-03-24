7800fb88f5b78cddef008777dfa16268c6ab45ff
nadia <nadia.gonzalez@bbconsult.co.uk>
2025-03-24 18:00:57 -0400

    exquisite_color_vortex piece added

---
+++ exquis-bac-a-sable/exquisite_color_vortex.js
@@ -0,0 +1,60 @@
+(() => {
+    let s, _height, _width;
+
+    const colors = [
+        [267, 52, 100],    // #937AFF - Lavender Purple
+        [54, 72, 97],      // #F8DB46 - Bright Yellow
+        [333, 76, 100],    // #FF3D9A - Bright Pink
+        [251, 86, 100]     // #5627FF - Deep Blue-Violet
+    ];
+
+    async function init() {
+        colorMode(HSB);
+        s = O_currentsection;
+        _height = O_sectionheight;
+        _width = O_sectionwidth;
+    }
+
+    function draw() {
+        push();
+        translate(s.x, s.y);
+        fill(0, 0, 0);
+        fill(0, 0, 0);
+
+        strokeWeight(4);
+        triangle(0, 0, s.x1, s.y1, s.x4, s.y4);
+        triangle(s.x1, s.y1, _width, 0, s.x2, s.y2);
+        triangle(s.x2, s.y2, _width, _height, s.x3, s.y3);
+        triangle(s.x3, s.y3, 0, _height, s.x4, s.y4);
+
+        draw_lines();
+
+        pop();
+    }
+
+    function draw_lines() {
+        let current_i = 0;
+
+        while (current_i < O_sectionduration) {
+            line(
+                _width / 2,
+                _height / 2, 
+                random(_width), 
+                random(_height)
+            );
+    
+            let randomColor = colors[Math.floor(random(colors.length))];        
+            stroke(randomColor[0], randomColor[1], randomColor[2]);
+            strokeWeight(Math.floor(random(2, 7)));
+    
+            triangle(0, 0, s.x1, s.y1, s.x4, s.y4);
+            triangle(s.x1, s.y1, _width, 0, s.x2, s.y2);
+            triangle(s.x2, s.y2, _width, _height, s.x3, s.y3);
+            triangle(s.x3, s.y3, 0, _height, s.x4, s.y4);
+
+            current_i++;
+        }
+    }
+  
+    window.exquisite_color_vortex = { init, draw };
+})();
