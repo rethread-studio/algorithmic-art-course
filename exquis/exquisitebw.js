60dd971b21e3fd2400aa5f17e68cfeb2da0942cd
bbaudry <baudry@kth.se>
2025-03-25 15:17:41 -0400

    update main exquisite infra

--- exquis/exquisitebw.js
+++ exquis/exquisitebw.js
@@ -1,26 +1,36 @@
-var s
-
-function initbw() {
-    s = O_currentsection
-}
-
-
-function drawbw() {
-    var r=random()
-    push()
-    translate(s.x, s.y)
-    fill(0,0,0)
-    rect(0,0,O_sectionwidth,O_sectionheight)
-    if(r>0.5){
-        line(s.x1,s.y1,s.x3,s.y3)
+(() => {
+    let s, r, rinc;
+  
+    async function init() {
+      s = O_currentsection;
+      r = 7;
+      rinc = 0.5;
+      console.log(s.x,s.y)
     }
-    else{
-        line(s.x2,s.y2,s.x4,s.y4)
+  
+    function draw() {
+      // Move to the section
+      push();
+      translate(s.x, s.y);
+  
+      // Create border around section
+      fill(0, 0, 0);
+      rect(0, 0, O_sectionwidth, O_sectionheight);
+      fill(0, 0, 100);
+      noStroke();
+  
+      // Draw our art
+      arc(s.x1, s.y1, r, r, 0, PI);
+      arc(s.x2, s.y2, r, r, PI * 0.5, PI * 1.5);
+      arc(s.x3, s.y3, r, r, PI, 2 * PI);
+      arc(s.x4, s.y4, r, r, PI * 1.5, PI * 0.5);
+      r += rinc;
+  
+      // Pop out of the section
+      pop();
     }
-    if(O_counter%O_sectionduration==O_sectionduration-1){
-        line(s.x1,s.y1,s.x3,s.y3)
-        line(s.x2,s.y2,s.x4,s.y4)
-
-    }
-    pop()
-}
+  
+    // Use the name of the current js file (without the extension) as the key in the object window.
+    window.exquisitebw = { init, draw };
+  })();
+  
