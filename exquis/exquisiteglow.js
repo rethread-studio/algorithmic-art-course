e381d00b6ce9bb1929e7fef983094642e326a406
KIRUTHIKA S <89388434+Kiruthika162003@users.noreply.github.com>
2025-04-06 22:09:04 -0300

    Update exquisiteglow.js

--- exquis/exquisiteglow.js
+++ exquis/exquisiteglow.js
@@ -1,14 +1,16 @@
 (() => {
-  let s;
-  let snowflakes = [];
+  let s, r, rinc, snowflakes = [];
 
   async function init() {
     s = O_currentsection;
-    for (let i = 0; i < 80; i++) {
+    r = 7;
+    rinc = 0.1;
+
+    for (let i = 0; i < 60; i++) {
       snowflakes.push({
         x: random(O_sectionwidth),
         y: random(O_sectionheight),
-        r: random(1, 3),
+        r: random(1, 2.5),
         speed: random(0.4, 1)
       });
     }
@@ -19,14 +21,16 @@
     push();
     translate(s.x, s.y);
 
-    drawingContext.beginPath();
-    drawingContext.rect(0, 0, O_sectionwidth, O_sectionheight);
-    drawingContext.clip();
+    // Border
+    fill(0, 0, 0);
+    rect(0, 0, O_sectionwidth, O_sectionheight);
+    noStroke();
 
+    // Background
     fill(235, 50, 30);
     rect(0, 0, O_sectionwidth, O_sectionheight);
 
-    // Trees
+    // Tree trunks & foliage
     for (let i = 0; i < 3; i++) {
       let x = (i + 1) * O_sectionwidth / 4;
       fill(30, 90, 40);
@@ -46,15 +50,13 @@
       }
     }
 
-    // Ground snow
-    fill(0, 0, 100, 120);
-    ellipse(O_sectionwidth / 2, O_sectionheight + 10, O_sectionwidth + 50, 40);
-
-    // Anchor point portal at bottom center (x3, y3)
-    let pulse = map(sin(frameCount * 0.05), -1, 1, 60, 100);
-    noStroke();
-    fill(220, 80, 100, 50);
-    ellipse(s.x3, s.y3, pulse, 20);
+    // Anchor point visuals
+    fill(0, 0, 100);
+    arc(s.x1, s.y1, r, r, 0, PI);
+    arc(s.x2, s.y2, r, r, PI * 0.5, PI * 1.5);
+    arc(s.x3, s.y3, r, r, PI, 2 * PI);
+    arc(s.x4, s.y4, r, r, PI * 1.5, PI * 0.5);
+    r += rinc;
 
     pop();
   }
