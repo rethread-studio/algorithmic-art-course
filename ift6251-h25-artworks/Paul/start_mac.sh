e65514cb43bc9ef58d3f7d426bb5c8cc2e276744
yogyagamage <47789154+yogyagamage@users.noreply.github.com>
2025-04-08 13:49:40 -0400

    Add mac scripts

--- ift6251-h25-artworks/Paul/start_mac.sh
+++ ift6251-h25-artworks/Paul/start_mac.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Hardcode de la liste des dossiers
-dossiers=("oeuvre_1" "oeuvre_2" "oeuvre_3" )
+dossiers=("FlowLake" "Life", "Pixel-Sorting")
 
 # Sélectionner un dossier au hasard
-dossier_choisi=$(echo "$dossiers" | shuf -n 1)
+#dossier_choisi=$(echo "$dossiers" | shuf -n 1)
+dossier_choisi=$(printf "%s\n" "${dossiers[@]}" | gshuf -n 1)
 
 # Vérifier si un dossier a été trouvé
 if [ -z "$dossier_choisi" ]; then
@@ -24,7 +25,7 @@ fi
 # Lancer la commande cmd.exe pour ouvrir index.html
 echo "Lancement de la commande dans le dossier : $dossier_choisi"
 
-cmd.exe /c start "" index.html # pour windows
-# open index.html # pour mac OS
+# cmd.exe /c start "" index.html # pour windows
+open index.html # pour mac OS
 # xdg-open index.html # pour linux 
 
