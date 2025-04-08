0e7879cccf220d312ff4fa9b67339ebe03e87f8a
Houda Fatih <houdafatih1999@gmail.com>
2025-04-08 11:27:39 -0400

    Modify ExquisiteShapeCycle

--- exquis/exquisiteShapeCycle.js
+++ exquis/exquisiteShapeCycle.js
@@ -4,49 +4,62 @@
   let morphSpeed = 0.02;
   let spiralFactor = 0;
   let currentHue = 0;
+  let baseRadius;
 
   async function init() {
-    s = O_currentsection; 
+    s = O_currentsection;
     colorMode(HSB, 360, 100, 100, 1);
     noFill();
+
+    baseRadius = min(O_sectionwidth, O_sectionheight) * 0.25;
   }
 
   function draw() {
     t += morphSpeed;
-    spiralFactor += 0.005;
-    currentHue = (currentHue + 0.5) % 360;
+    spiralFactor += 0.01;
+    currentHue = (currentHue + 2) % 360;
 
-    let pulse = sin(t * 1.5) * 0.5 + 0.5;
-    let baseRadius = 180 * pulse;
-    let sides = floor(map(sin(t), -1, 1, 3, 10));
-    let shapeRotation = t * 0.3;
+    let centerX = s.x + O_sectionwidth / 2;
+    let centerY = s.y + O_sectionheight / 2;
 
+    // Optional: draw semi-transparent rect to slowly fade trails
+    noStroke();
+    fill(0, 0, 0, 0.05);
+    rect(centerX - O_sectionwidth / 2, centerY - O_sectionheight / 2, O_sectionwidth, O_sectionheight);
 
+    // Draw multiple offset shapes for the glitch effect
+    for (let j = 0; j < 5; j++) {
+      let localT = t + j * 0.1;
+      let pulse = sin(localT * 1.5) * 0.5 + 0.5;
+      let sides = floor(map(sin(localT), -1, 1, 3, 10));
+      let shapeRotation = localT * 0.3;
 
-    push();
-    translate(s.x, s.y);
-    fill(0, 0.1); 
-    rect(0, 0, O_sectionwidth, O_sectionheight);
-    rotate(shapeRotation);
+      push();
+      translate(centerX, centerY);
+      rotate(shapeRotation);
 
-    stroke(currentHue, 80, 100, 0.8);
-    strokeWeight(2);
-    beginShape();
-    for (let i = 0; i <= sides; i++) {
-      let angle = map(i, 0, sides, 0, TWO_PI);
-      let radiusVariation = noise(i * 0.2, t) * 50 * pulse;
-      let radius = baseRadius + radiusVariation;
+      stroke((currentHue + j * 40) % 360, 100, 100, 0.8);
+      strokeWeight(2);
+      noFill();
+      beginShape();
+      for (let i = 0; i <= sides; i++) {
+        let angle = map(i, 0, sides, 0, TWO_PI);
+        let radiusVariation = noise(i * 0.3, localT) * 40 * pulse;
+        let radius = baseRadius + radiusVariation;
 
-      angle += spiralFactor * 20 / (radius + 1);
-      let x = cos(angle) * radius;
-      let y = sin(angle) * radius;
+        angle += spiralFactor * 20 / (radius + 1);
 
-      vertex(x, y);
+        let x = cos(angle) * radius;
+        let y = sin(angle) * radius;
+        vertex(x, y);
+      }
+      endShape(CLOSE);
+      pop();
+     
     }
-    endShape(CLOSE);
-
-    pop();
   }
 
   window.exquisiteShapeCycle = { init, draw };
 })();
+
+
